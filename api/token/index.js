42174f7a8a1973a20a78615eca975f0b261e3c23
rgwyatt24 <rgwyatt24@gmail.com>
2025-05-27 08:43:27 -0700

    Update index.js

--- api/token/index.js
+++ api/token/index.js
@@ -1,22 +1,28 @@
+import fetch from 'node-fetch';
+
 export default async function handler(req, res) {
-  const response = await fetch("https://identity.passkit.io/connect/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: new URLSearchParams({
-      client_id: "4E5tdObW30jEiZdyFs9fL9uA",
-      client_secret: "47RCjidzZPlzOkMnvd6vv3osWvUbCGXtEapcB0pO",
-      grant_type: "client_credentials",
-      scope: "api"
-    })
-  });
+  try {
+    const response = await fetch("https://identity.passkit.io/connect/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: new URLSearchParams({
+        client_id: "4ESldObW3QiEzdyFsf9LUa",
+        client_secret: "47RCJidZ0P1zOkMnvd6vv3osWVuDcGXtEapcBOpO",
+        grant_type: "client_credentials",
+        scope: "api"
+      })
+    });
 
-  const data = await response.json();
+    const data = await response.json();
 
-  if (!response.ok) {
-    return res.status(response.status).json({ error: data });
+    if (!response.ok) {
+      return res.status(response.status).json({ error: data });
+    }
+
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json({ error: "Internal server error", details: err.message });
   }
-
-  return res.status(200).json(data);
 }
