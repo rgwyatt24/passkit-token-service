15ea63752b9e96eccf624978983e156f29eee6d8
rgwyatt24 <rgwyatt24@gmail.com>
2025-05-27 08:54:42 -0700

    Update index.js

--- api/token/index.js
+++ api/token/index.js
@@ -1,24 +1,22 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
   try {
     const response = await fetch("https://identity.passkit.com/connect/token", {
       method: "POST",
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       body: new URLSearchParams({
         client_id: "4ESldObW3QiEzdyFsf9LUa",
         client_secret: "47RCJidZ0P1zOkMnvd6vv3osWVuDcGXtEapcBOpO",
         grant_type: "client_credentials",
-        scope: "api"
-      })
+        scope: "api",
+      }),
     });
 
     const data = await response.json();
 
     if (!response.ok) {
-      return res.status(response.status).json({ error: data });
+      return res.status(response.status).json({ error: "Failed to fetch token", details: data });
     }
 
     return res.status(200).json(data);
